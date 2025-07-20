577d579eeae5ffff7897bea34d70a043b3fc4871
Ashish <azk640@psu.edu>
2025-07-20 16:05:46 -0400

    Added input example

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from graph import app
 
 initial_state = {
     "image_path": "data/sample.png",
-    "user_question": "What is written on the whiteboard?" #Or get one from prompts/question.txt
+    "user_question": "What is written on the whiteboard?" , #Or get one from prompts/question.txt
+    "external_facts": "Nothing so far."
 }
 
 result = app.invoke(initial_state)
